fa9cfdc3e64cb319acd4b423835bbc40c1489af5
Christopher Adams <chrismichaeladams@gmail.com>
2019-01-26 12:39:59 -0500

    Start working on looping through linked list

--- CodePuzzles/addingTwoNumbersLinkedLists/index.js
+++ CodePuzzles/addingTwoNumbersLinkedLists/index.js
@@ -11,6 +11,7 @@ function ListNode(val) {
 var l1 = new ListNode(2);
 l1.next = new ListNode(4)
 l1.next.next = new ListNode(3);
+//l1.next.next.next = new ListNode(5);
 
 var l2 = new ListNode(5);
 l2.next = new ListNode(6);
